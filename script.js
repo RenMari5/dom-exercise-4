4da7d7154c10696ae429a5d2ceee59e49bcc01f2
Renee Paczkowski <rpaczkowski@gmail.com>
2022-05-19 19:15:48 -0400

    Over-thought the coin sizes

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-const submitCoin = document.getElementById("submit-button");
+const submitCoin = document.getElementById("coin-form");
+
+submitCoin.addEventListener("submit", handleCoinSubmit);
+
+function handleCoinSubmit(event) {
+  event.preventDefault();
+  const coinInput = document.querySelector("");
+}
