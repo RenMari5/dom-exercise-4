1084c1730ef5fd79274ce8b44eee71ab04c244d0
shelbysue <101065139+shelbysue@users.noreply.github.com>
2022-05-19 20:19:51 -0400

    added coins in for loop

--- script.js
+++ script.js
@@ -7,4 +7,17 @@ function handleCoinSubmit(event) {
   const addCoinNumber = document.getElementById("how-many").value;
 
   const getCoin = document.getElementById("which-coin").value;
+  
+  for (let i = 0; i <= addCoinNumber; i++){
+    let listCoins = document.createElement("li");
+    if (getCoin === "Penny"){
+      listCoins.innerText = getCoin;
+    } else if (getCoin === "Dime"){
+      listCoins.innerText = getCoin;
+    } else if (getCoin === "Quarter"){
+      listCoins.innerText = getCoin;
+    } else if (getCoin === "Nickle"){
+      listCoins.innerText = getCoin;
+    }
+  } 
 }
